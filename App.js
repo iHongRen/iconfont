1be5eda246b0f8f54ed56cd1a26147777ad60711
cxy <iamhongren@gmail.com>
2018-07-24 18:14:25 +0800

    UI fixed

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ import {
   Dimensions,
   ScrollView,
   TouchableOpacity,
-  AsyncStorage
+  AsyncStorage,
+  TextInput
 } from 'react-native';
 import * as opentype from 'opentype.js';
 
@@ -39,22 +40,23 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    AsyncStorage.getItem(FileStorageKey).then(filesJosn => {
-      files = JSON.parse(filesJosn);
-      if (files !== null) {
-        this.setState({
-          files: files
-        });
-      }
-    })
+    this.getStorageFile();
+  }
+
+  renderAppNameView() {
+    return (
+      <View style={styles.appNameWrap}>
+        <Text style={styles.appName}>Iconfont 1.0</Text>
+      </View>
+    );
   }
 
   renderDragView() {
     const { files, dragOver, mouseOver  } = this.state;
-    const dragColor =  dragOver ? 'yellow' : (mouseOver ? 'orange' : 'white');
+    const dragColor =  dragOver ? 'yellow' : (mouseOver ? 'orange' : '#F5FCFF');
     return (
       <View
-        style={[styles.dragView ,{backgroundColor: dragColor}]}
+        style={[styles.fullDragView ,{backgroundColor: dragColor}]}
         draggedTypes={['NSFilenamesPboardType']}
         onMouseEnter={() => this.setState({mouseOver: true})}
         onMouseLeave={() => this.setState({mouseOver: false})}
@@ -71,59 +73,90 @@ export default class App extends Component {
   renderFontFileView() {
     const { files } = this.state;
     return (
-      <ScrollView style={styles.fontFileWrap} contentContainerStyle={styles.fontFileContainerWrap}>
+      <ScrollView 
+      horizontal  
+      style={fileItemStyles.fontFileWrap}
+      contentContainerStyle={fileItemStyles.fontFileContainerItem} 
+      >
         {
           files.map((e, index)=> { return (
-            <View key={index} style={styles.fileItemWrap}>
-              <View style={styles.fileItem}>
-                <TouchableOpacity style={styles.fileIconWrap} onPress={()=> this.onFileSelected(index)}>
-                  <Text>图片</Text>
-                </TouchableOpacity>
-                <View style={styles.fileNameWrap}>
-                  <Text style={styles.fileName}>{e.split('/').pop()}</Text>
+            <View key={index} style={fileItemStyles.fileItemWrap}>
+              <TouchableOpacity activeOpacity={0.7} style={fileItemStyles.fileItem} onPress={()=> this.onFileSelected(index)}>
+                <View style={fileItemStyles.fileIconWrap}>
+                  <Text style={{fontSize: 35, color: '#ddd'}}>𝐹</Text>
+                </View>
+                <View style={fileItemStyles.fileNameWrap}>
+                  <Text style={fileItemStyles.fileName} numberOfLines={1}>{e.split('/').pop()}</Text>
                 </View>             
-              </View>
-              <TouchableOpacity style={styles.fileClose} onPress={()=> this.onDeleteFileItem(index)}>
-                <Text style={styles.fileCloseIcon}>✕</Text>
+              </TouchableOpacity>
+              <TouchableOpacity activeOpacity={0.7} style={fileItemStyles.fileClose} onPress={()=> this.onDeleteFileItem(index)}>
+                <Text style={fileItemStyles.fileCloseIcon}>✕</Text>
               </TouchableOpacity>                                         
             </View>
             )}
           )        
         }
       </ScrollView>
+    );    
+  }
+
+  renderJsonMapperView(glyphs) {
+    const jsonText = this.parseMapperGlyphs(glyphs);
+    return (
+      <View style={panelStyles.mapperView}>
+<ScrollView >
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+          <Text style={{flex: 1, backgroundColor: '#fff'}} selectable={true}>
+            {jsonText}
+          </Text>
+        </View>
+      </ScrollView>
+      </View>
+      
+      // <View style={panelStyles.mapperView}>
+      //   <TextInput 
+      //   style={panelStyles.jsonView}
+      //   editable={false}
+      //   multiline
+      //   value={jsonText}
+      //   />
+      // </View> 
     );
-    
+  }
+
+  renderPanelView() {
+    const { glyphs } = this.state;
+    const { files, dragOver, mouseOver  } = this.state;
+    const dragColor =  dragOver ? 'orange' :  '#ddd';
+    let html = this.getGlyphWrapperHtml(glyphs);
+    return (
+      <View style={panelStyles.panelView}>
+        <View style={[panelStyles.headerView, {borderColor: dragColor}]}
+            draggedTypes={['NSFilenamesPboardType']}
+            onMouseEnter={() => this.setState({mouseOver: true})}
+            onMouseLeave={() => this.setState({mouseOver: false})}
+            onDragEnter={() => this.setState({dragOver: true})}
+            onDragLeave={() => this.setState({dragOver: false})}
+            onDrop={(e) => this.onDrop(e)}
+          >
+          { this.renderFontFileView() }
+        </View>
+
+        <View style={panelStyles.fontShowView}>         
+          <View style={panelStyles.fontShow}>
+            <WebView source={{html:html}} />
+          </View> 
+          { this.renderJsonMapperView(glyphs) }          
+        </View>
+      </View>
+    );
   }
 
   render() {  
-    const { glyphs } = this.state;
-    const { files, dragOver, mouseOver  } = this.state;
-    const dragColor =  dragOver ? 'yellow' : (mouseOver ? 'orange' : 'white');
-    let html = this.getGlyphWrapperHtml(glyphs);
     return (
       <View style={styles.container}> 
-        <View style={[styles.headerView, {borderColor: dragColor}]}
-          draggedTypes={['NSFilenamesPboardType']}
-          onMouseEnter={() => this.setState({mouseOver: true})}
-          onMouseLeave={() => this.setState({mouseOver: false})}
-          onDragEnter={() => this.setState({dragOver: true})}
-          onDragLeave={() => this.setState({dragOver: false})}
-          onDrop={(e) => this.onDrop(e)}
-        >
-          {/* { this.renderDragView() } */}
-          { this.renderFontFileView() }
-        </View>
-        <View style={styles.fontShowView}>         
-          <View style={styles.fontShow}>
-            <WebView
-              style={styles.webView}
-              scrollEnabled
-              source={{html:html}}
-            />
-          </View> 
-          <View style={styles.mapperView}>
-          </View> 
-        </View>                        
+        { this.renderAppNameView() }
+        { (this.state.files.length === 0) ? this.renderDragView() : this.renderPanelView() }
       </View>
     );
   }
@@ -146,11 +179,11 @@ export default class App extends Component {
     let files = e.nativeEvent.files;
     files = this.filteredTTFFiles(files);
 
-    AsyncStorage.setItem(FileStorageKey, JSON.stringify(files));
-
     this.setState({
       files: files, 
       dragOver: false
+    }, ()=> {
+      this.setStorageFile();
     });
 
     const file = files[0];
@@ -177,11 +210,10 @@ export default class App extends Component {
     let glyphs = []    
     for (let i = 0; i < fontGlyphs.length; i++) {
       let glyph = fontGlyphs.get(i);
-      if (glyph.unicode) {
-        let path = glyph.getPath(0,40,40);
-        glyph.svg = path.toSVG();;
-        glyphs.push(glyph); 
-      }        
+      let path = glyph.getPath(0, 40, 40);
+      glyph.svg = path.toSVG();
+      glyph.hex = this.toHex(glyph.unicode);
+      glyphs.push(glyph); 
     }
     
     this.setState({
@@ -190,15 +222,32 @@ export default class App extends Component {
     console.log(glyphs);
   } 
 
+  toHex(unicode) {
+    if(unicode == undefined) return undefined;
+    let hex = unicode.toString(16);  
+    while(hex.length < 4) {
+      hex = '0' + hex; 
+    } 
+    return hex;
+  } 
+
+  parseMapperGlyphs(glyphs) {
+    const mapperText = glyphs.filter(e=> e.name && e.unicode).map(e=> `  ${e.name}: ${e.unicode},`).join('\n');
+    return `{\n${mapperText}\n}`;
+  }
+
   getGlyphHtml(glyph) {
-    const { svg, name, unicode } = glyph;
+    const { svg, name, hex, unicode, index } = glyph;
+    const glyphTop = index < 3 ? 'glyph-top' : '';
+    const glyphLeft = index%3 === 0 ? 'glyph-left' : '';
     return (
-      `<div class="glyph">
-        <svg class="glyph_svg">          
+      `<div class="glyph ${glyphTop} ${glyphLeft}">
+        <div> ${index} </div>
+        <svg class="glyph-svg">          
           ${svg}
         </svg>
         <p> ${name} </p>
-        <p> ${unicode}</p>
+        <p> ${hex}  /  ${unicode}</p>
       </div>`
     );
   }
@@ -212,45 +261,64 @@ export default class App extends Component {
       `
     );
   }
+
+  getStorageFile() {
+    AsyncStorage.getItem(FileStorageKey).then(filesJosn => {
+      files = JSON.parse(filesJosn);
+      if (files !== null) {
+        this.setState({
+          files: files
+        });
+      }
+    });
+  }
+
+  setStorageFile() {
+    let files = this.state.files;
+    AsyncStorage.setItem(FileStorageKey, JSON.stringify(files));
+  }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    overflow: 'hidden'
-  },
-  headerView: {
-    marginTop: 40,
-    marginHorizontal: 20,
-    flexDirection: 'row',
-    backgroundColor: 'green',
-    borderStyle: 'solid',
-    borderWidth: 3,
-    height: 150,
-  },
-  dragView: {
-    padding: 10,
-    width: 120,
-   
-    alignItems: 'center',
-    borderColor: '#ddd',
-    borderWidth: 1
-  },
+const fileItemStyles = StyleSheet.create({
   fontFileWrap: {
-    marginHorizontal: 10,
-    marginBottom: 10,
-    backgroundColor: 'red'
+    flex: 1,
+    marginHorizontal: 10,   
   },
-  fontFileContainerWrap: {
-    flexDirection: 'row',   
+  fontFileContainerItem: {
+    alignItems: 'center',
   },
   fileItemWrap: {
     width: 100,
     marginRight: 15,
-    marginTop: 5,
-    marginBottom: 10
+    marginTop: 10
+  },  
+  fileItem: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },  
+  fileIconWrap: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#ddd',
+    borderWidth: 1,
+    width: 80,
+    height: 100,
+    marginTop: 5
+  },
+  fileIcon: {
+    borderColor: '#ddd',
+    borderWidth: 1,
+    fontSize: 15
+  },
+  fileNameWrap: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 5
+  },
+  fileName: {
+    fontSize: 14,
+    color: '#333',
+    textAlign: 'center',
   },
   fileClose: {
     alignSelf: 'flex-end',
@@ -271,37 +339,27 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: '#fff'
   },  
-  fileItem: {
-    marginTop: 10,
-    marginRight: 10
-  },  
-  fileIconWrap: {
-    alignItems: 'center',
-    borderColor: '#ddd',
+});
+
+const panelStyles = StyleSheet.create({
+  panelView: {
+    flex: 1,
+  },
+  headerView: {
+    marginTop: 0,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+    borderStyle: 'solid',
     borderWidth: 1,
-    height: 100
-  },
-  fileIcon: {
-    borderColor: '#ddd',
-    borderWidth: 1,
-    fontSize: 15
-  },
-  fileNameWrap: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 10
-  },
-  fileName: {
-    fontSize: 15,
-    color: '#333'
+    height: 150,
+    backgroundColor: '#fff'
   },
   fontShowView: {
     flex: 1,
-    marginVertical: 20,
-    marginHorizontal: 20,
+    marginVertical: 15,
+    marginHorizontal: 15,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    backgroundColor: 'red'
   },
   fontShow: {
     flex: 1,
@@ -311,40 +369,61 @@ const styles = StyleSheet.create({
     flexGrow: 2,
     backgroundColor: '#0f0'
   },
-  webView: {
-    flex: 1,
-    backgroundColor: '#000'
-  },
   mapperView: {
+    flex: 1,
     flexGrow: 1,
-    marginLeft: 20,
+    marginLeft: 15,
     marginTop: 0,
     marginBottom: 0,
-    backgroundColor: '#ff0'
+    flexWrap: 'wrap',
+    paddingVertical: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  jsonView: {
+    color: 'orange',
+    fontSize: 15,
+    height: 150,
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    overflow: 'hidden'
   },
+  appNameWrap: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  appName: {
+    marginVertical: 5,
+    fontSize: 15,
+    color: '#333',
+  },  
+  fullDragView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
 const glyphWrapStyles = `
 <style> 
   .glyph {
-    padding: 20px;
+    padding: 10px 15px 10px 15px;
     width: 33.333333%;
-    border-bottom-style: solid;
-    border-right-style: solid;
-    border-right-width: 1px;
-    border-bottom-width: 1px;
-    border-color: lightgray;
-    -webkit-box-sizing:border-box;
-    border-width: 1px;    
-    border-style: solid;      
-  }    
-  .glyph_svg {
-    border:1px solid #cd0000;
+    border-bottom: 1px solid lightgray;
+    border-right: 1px solid lightgray; 
+    -webkit-box-sizing:border-box;     
+  }
+  .glyph-top {
+    border-top: 1px solid lightgray; 
+  }
+  .glyph-left {
+    border-left: 1px solid lightgray; 
+  }  
+  .glyph-svg {
+    margin-top: 10px;
     width: 50px;
     height: 50px;       
   }
